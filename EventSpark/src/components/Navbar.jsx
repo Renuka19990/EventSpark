8e6c983cd5eeea6d8abefe3e9846b2eff218f183
Aman Chaubey <amanchaubey86@gmail.com>
2024-05-10 00:30:04 +0530

    footer's ui

--- EventSpark/src/components/Navbar.jsx
+++ EventSpark/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import logo from '../assets/logo.jpeg'
+import logo from '../assets/logo.png'
 import {
   Box,
   Flex,
--- EventSpark/src/routes/AllRoutes.jsx
+++ EventSpark/src/routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "../pages/HomePage"
 import FindEvents from "../pages/FindEvents"
 import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 const AllRoutes = () => {
   return (
@@ -12,6 +13,7 @@ const AllRoutes = () => {
         <Route path="/findevent" element={<FindEvents/>}/>
         <Route/>
       </Routes>
+        <Footer/>
     </div>
   )
 }

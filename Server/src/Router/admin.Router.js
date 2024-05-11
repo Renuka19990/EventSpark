73811873953cac8dc251814080e2ba08b355c10b
Anuj <anujkshatriya82@gmail.com>
2024-05-11 13:51:00 +0530

    Functinality Added

---
+++ Server/src/Router/admin.Router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { deleteUser, updateUser, userData, getUsers, AddUser } = require("../Controller/admin.controller");
+
+
+const adminRouter = express.Router();
+
+adminRouter.post("/add", AddUser);
+
+adminRouter.get("/users", getUsers);
+
+adminRouter.get("/user/:id", userData);
+
+adminRouter.patch("/users", updateUser);
+
+adminRouter.delete("/users", deleteUser);
+
+module.exports = { adminRouter };

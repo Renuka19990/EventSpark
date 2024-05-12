da77acf1237510ea22aed1bac4d0c6da9b96eca8
Anuj <anujkshatriya82@gmail.com>
2024-05-13 02:40:49 +0530

    find Events Page Added

--- EventSpark/src/Admin/Context/ThemeContext.jsx
+++ EventSpark/src/Admin/Context/ThemeContext.jsx
@@ -1,18 +1,152 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext();
+
+const userRes = {
+  isAuth: false,
+  token: "",
+  isAdmin: false,
+};
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [LoggedIn, setLoggedIn] = useState(userRes);
+
+  const handleLogin = async ({ email, password }) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        console.log(email, password);
+        const res = await axios.post("http://localhost:8080/login", {
+          email,
+          password,
+        });
+        // setAuth(res.data.isAuth);
+        if (res) {
+          setLoggedIn({
+            isAuth: true,
+            token: res.data.accessToken,
+            isAdmin: res.data.isAdmin,
+          });
+        }
+        console.log(res.data.accessToken);
+        localStorage.setItem("accessToken", res.data.accessToken);
+        resolve();
+      } catch (error) {
+        console.log(error);
+        reject();
+      }
+    });
+  };
+
+  const handleSignUp = async (obj) => {
+    try {
+      const res = await axios.post("http://localhost:8080/register", obj);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = async () => {
+            try {
+              const token = localStorage.getItem("accessToken");
+              const res = await axios.get("http://localhost:8080/logout", {
+                headers: {
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+              console.log(res);
+              setLoggedIn(userRes);
+              localStorage.removeItem("accessToken");
+            } catch (error) {
+              console.log(error);
+            }
+          };
+
 
   const login = () => setIsLoggedIn(true);
   const logout = () => setIsLoggedIn(false);
-//
+  //
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout,handleLogout,handleSignUp,handleLogin,LoggedIn,setLoggedIn }}>
       {children}
     </AuthContext.Provider>
   );
 };
+
+// import { useState } from "react";
+// import { createContext } from "react";
+// import axios from "axios";
+
+//   const userRes={
+//     isAuth:false,
+//     token:"",
+//     isAdmin:false
+//   }
+// export const AuthContext =createContext();
+// export const AuthContextProvider=({children})=>{
+//       const [isLoggedIn, setLoggedIn] = useState(userRes);
+
+//       const handleLogin = async({ email, password }) => {
+//          return new Promise(async(resolve,reject)=>{
+//             try {
+//                 console.log(email,
+//                   password)
+//                 const res = await axios.post("http://localhost:3000/user/login", {
+//                   email,
+//                   password,
+//                 });
+//                 // setAuth(res.data.isAuth);
+//                 if(res){
+//                   setLoggedIn({
+//                     isAuth:true,
+//                     token:res.data.accessToken,
+//                     isAdmin:res.data.isAdmin
+//                   })
+//                 }
+//                 console.log(res.data.accessToken)
+//                 localStorage.setItem("accessToken",res.data.accessToken);
+//                 resolve();
+//               } catch (error) {
+//                 console.log(error);
+//                 reject();
+//               }
+//             ;
+//          })
+
+//       };
+
+//       console.log(isLoggedIn);
+//       const handleSignup = async (obj) => {
+//           try {
+//             const res = await axios.post("http://localhost:3000/user/register", obj);
+//             console.log(res.data);
+//           } catch (error) {
+//             console.log(error);
+//           }
+//       };
+
+//       const handleLogout = async () => {
+//         try {
+//           const token = localStorage.getItem("accessToken");
+//           const res = await axios.get("http://localhost:3000/user/logout", {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//             },
+//           });
+//           console.log(res);
+//           setLoggedIn(userRes);
+//           localStorage.removeItem("accessToken");
+//         } catch (error) {
+//           console.log(error);
+//         }
+//       };
+
+//     return(
+//         <AuthContext.Provider value={{handleSignup,handleLogin,handleLogout,isLoggedIn}}>
+//              {children}
+//         </AuthContext.Provider>
+//     )
+// }
--- EventSpark/src/App.jsx
+++ EventSpark/src/App.jsx
@@ -2,15 +2,13 @@
 import AllRoutes from "./routes/AllRoutes";
 
 import './App.css'
-import { EventPage } from "./Eventpage/page"
+
 
 
 const App = () => {
 
   return (
     <div>
-
-     <EventPage/>
       <AllRoutes/>
       
     </div>
